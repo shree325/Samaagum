00bbde4c143ccd8fa986ce4c3e13bafb907c9cff
shree325 <0801ca231137@sgsits.ac.in>
2026-07-23 12:11:49 +0530

    Fixes on createEventForm

--- client/src/create-event/CreateEventForm.tsx
+++ client/src/create-event/CreateEventForm.tsx
@@ -14,6 +14,7 @@ import { AccessControlModal } from './features/Access/AccessControlModal';
 import { TicketSettingsModal } from './features/Ticketing/TicketSettingsModal';
 import { QuestionnaireModal } from './features/Questionnaire/QuestionnaireModal';
 import { Toggle } from './ui/Toggle';
+import { HtmlRenderer } from '../components/HtmlRenderer';
 
 import { format24to12, getDurationText, getTzInfo, parse12to24, addOneHour } from './utils/time';
 import { getSelectedNodesWithDetails } from './utils/access-tree';
