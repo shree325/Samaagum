978c88eebd6b352c73b093ee12c6cfa067c53313
shree325 <0801ca231137@sgsits.ac.in>
2026-07-24 11:45:16 +0530

    Create form fix

--- client/src/create-event/CreateEventForm.tsx
+++ client/src/create-event/CreateEventForm.tsx
@@ -16,9 +16,6 @@ import { QuestionnaireModal } from './features/Questionnaire/QuestionnaireModal'
 import { Toggle } from './ui/Toggle';
 import { HtmlRenderer } from '../components/HtmlRenderer';
 import { RichTextEditor } from '../components/RichTextEditor';
-
-import { HtmlRenderer } from '../components/HtmlRenderer/HtmlRenderer';
-import { RichTextEditor } from '../components/RichTextEditor';
 import { format24to12, getDurationText, getTzInfo, parse12to24, addOneHour } from './utils/time';
 import { getSelectedNodesWithDetails } from './utils/access-tree';
 import { ACCESS_TREE, TIMEZONES, DEFAULT_FREE_ENTITLEMENTS } from './constants';
